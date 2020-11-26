691c4ab6d79d62871c37f10c68530921786e5d18
TAILLANDIER Rémy <remytaillandier@hotmail.fr>
2020-11-26 19:27:19 +0100

    j'essaye d'utiliser l'annotation @SendToUser

---
+++ serveur/discord/src/main/java/com/rtaillandier/discord/controllers/ControllerWebSocketsSendToUser.java
@@ -0,0 +1,27 @@
+package com.rtaillandier.discord.controllers;
+
+import java.security.Principal;
+
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ControllerWebSocketsSendToUser {
+	
+	@MessageMapping("/sendtouser/message")
+    @SendToUser("/queue/reply")
+    public String processMessageFromClient(@Payload String message, Principal principal) throws Exception {
+		System.out.println(principal.getName()+" : "+message);
+        return message;
+    }
+
+    @MessageExceptionHandler
+    @SendToUser("/queue/errors")
+    public String handleException(Throwable exception) {
+        return exception.getMessage();
+    }
+
+}
